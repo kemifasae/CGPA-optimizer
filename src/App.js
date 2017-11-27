4a362fb89ec07bbcc7d030e8114bd4ee7dcb27db
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2017-11-27 13:49:56 +0100

    incorporate the components into the app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bgImage from './bgImage.jpg';
+import './components/css/bootstrap.min.css';
+
+import Header from './components/header/header';
+import Previous from './components/prevDetails/previous';
+import Current from './components/current/current';
+import Pix from './components/pix/pix';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+
+          <div className="row">
+            <div className="col-md-6">
+              <Previous />
+            </div>
+            <div className="col-md-6">
+              <img className="img-one" src={bgImage} alt ="bgImage" />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              <Current />
+            </div>
+            <div className="col-md-6">
+              <Pix />
+            </div>
+          </div>
       </div>
     );
   }
